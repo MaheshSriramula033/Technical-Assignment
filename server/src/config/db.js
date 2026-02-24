6cfb3b8a2a5ff6d8f526b5f84a0e1aa16cc4db7a
Mahesh <maheshsriramula033@gmail.com>
2026-02-24 21:01:46 +0530

    Initial project setup

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,18 +1,25 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const fs = require("fs");
-require("dotenv").config();
 
+let sslConfig = undefined;
+
+/* ---------- PRODUCTION DB SSL ---------- */
+if (process.env.DB_SSL === "true") {
+  sslConfig = {
+    rejectUnauthorized: true
+  };
+}
+
+/* ---------- CREATE POOL ---------- */
 const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  ssl: {
-    ca: fs.readFileSync(__dirname + "/../../certs/ca.pem")
-  }
+  ssl: sslConfig
 });
 
-module.exports = db.promise();
+module.exports = db;
