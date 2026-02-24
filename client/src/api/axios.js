8dbb2c2f5532cf3288a3d3a0a7374411ab4873e7
Mahesh <maheshsriramula033@gmail.com>
2026-02-24 21:16:45 +0530

    Initial project setup

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { refresh } from "./auth";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL, // ← dynamic,
 });
 
 /* attach access token */
